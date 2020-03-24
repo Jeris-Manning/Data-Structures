36431fff01ddda8f8fa5395d7ccf17bf7fca1b4c
Jeris Manning <the.goose.moose@gmail.com>
2020-03-24 15:24:59 -0600

    constructors set up properly, queue and stack fully functional

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -4,17 +4,17 @@ from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
-    def __init__(self, node=None):
+    def __init__(self):
         self.size = 0
-        self.storage = DoublyLinkedList(self)
-        self.head = node
-        self.tail = node
-        self.length = 1 if node is not None else 0
+        self.storage = DoublyLinkedList()
+        # self.head = node
+        # self.tail = node
+        # self.length = 1 if node is not None else 0
 
     def enqueue(self, value):
         self.size += 1
         self.storage.add_to_tail(value)
-        self.value = value
+        # self.value = value
 
 
 
@@ -24,8 +24,8 @@ class Queue:
             return None
         else:
             self.size -= 1
-            self.storage.remove_from_head()
-            return
+            return self.storage.remove_from_head()
+
 
     def len(self):
         return self.size
